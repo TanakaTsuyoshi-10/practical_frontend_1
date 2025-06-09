a3fe9da87cbf3283a9d47571e6c90f5772c0b44b
TanakaTsuyoshi-10 <fl73922@gmail.com>
2025-06-10 05:25:50 +0900

    修正5

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@ require('dotenv').config();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',  // ← 追加：Azureデプロイ用に必要
+  output: 'standalone',
   env: {
-    // .env ファイルから環境変数をビルド時に注入
     API_ENDPOINT: process.env.API_ENDPOINT,
   },
 };
